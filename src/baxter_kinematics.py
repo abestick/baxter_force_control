3a112f9e6511eb3f4ba85274a82cb9061139d9c3
Aaron Bestick <abestick@gmail.com>
2017-02-17 19:03:36 -0800

    Half-finished cartesian impedance controller

--- src/baxter_kinematics.py
+++ src/baxter_kinematics.py
@@ -9,6 +9,47 @@ from math import sqrt
 WRENCH = [10,0,0,0,0,0]
 JOINTS = ['right_s0', 'right_s1', 'right_e0', 'right_e1', 'right_w0', 'right_w1', 'right_w2']
 
+class CartesianImpedanceController:
+	def __init__(self, limb):
+		# create our limb instance
+        self._limb = baxter_interface.Limb(limb)
+
+        # initialize parameters
+        self._springs = dict()
+        self._damping = dict()
+        self._start_angles = dict()
+        self._rate = 1000.0
+        self._missed_cmds = 20.0
+        self._limb_name = limb
+
+        # create cuff disable publisher
+        cuff_ns = 'robot/limb/' + limb + '/suppress_cuff_interaction'
+        self._pub_cuff_disable = rospy.Publisher(cuff_ns, Empty, queue_size=1)
+
+        # verify robot is enabled
+        print("Getting robot state... ")
+        self._rs = baxter_interface.RobotEnable()
+        self._init_state = self._rs.state().enabled
+        print("Enabling robot... ")
+        self._rs.enable()
+        print("Running. Ctrl-c to quit")
+
+
+    def move_to_neutral(self):
+        """
+        Moves the limb to neutral location.
+        """
+        self._limb.move_to_neutral()
+
+    def clean_shutdown(self):
+        """
+        Switches out of joint torque mode to exit cleanly
+        """
+        print("\nExiting example...")
+        self._limb.exit_control_mode()
+        if not self._init_state and self._rs.state().enabled:
+            print("Disabling robot...")
+            self._rs.disable()
 
 def main():
     rospy.init_node('baxter_kinematics')
@@ -27,8 +68,7 @@ def main():
         limb.set_joint_torques(torques_dict)
         rate.sleep()
 
-
-    
+def get_null_space_torques()
 
 if __name__ == "__main__":
     main()
