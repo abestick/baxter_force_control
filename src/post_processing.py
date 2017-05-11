7fe66e238858cea76be2e586159d1234b442529c
Peter w <worsnop.p@gmail.com>
2017-05-11 14:15:55 -0700

    plotting

--- src/post_processing.py
+++ src/post_processing.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+import rospy
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
+from collections import OrderedDict
+from scipy.signal import medfilt
 
 
 class PlottableMocapData(object):
@@ -13,14 +17,17 @@ class PlottableMocapData(object):
         self.goal = goal
 
     def __getitem__(self, tup):
-        trials, markers, dimensions, time = unpad(tup, *[slice(None)]*3+[slice(-np.inf, np.inf)])
+        trials, markers, dimensions, time = pad_tuple(tup, *[slice(None)] * 3 + [slice(-np.inf, np.inf)])
         plot_list = []
 
         if not isinstance(time, slice):
-            raise TypeError("Time indexing must be via a slice!")
+            raise TypeError("Time indexing must be a slice!")
 
-        if isinstance(trials, int):
-            trials = slice(trials, trials+1)
+        if time.start is None:
+            time = slice(-np.inf, time.stop)
+
+        if time.stop is None:
+            time = slice(time.stop, np.inf)
 
         if isinstance(markers, int):
             markers = slice(markers, markers+1)
@@ -28,25 +35,21 @@ class PlottableMocapData(object):
         if isinstance(dimensions, int):
             dimensions = slice(dimensions, dimensions+1)
 
-        if isinstance(trials, (list, tuple, np.ndarray)):
-            marker_trials = []
-            time_trials = []
+        if isinstance(trials, int):
+            trials = [trials]
 
-            for t in trials:
-                marker_trials.append(self.marker_data[t])
-                time_trials.append(self.time_data[t])
+        elif isinstance(trials, slice):
+            trials = range(*trials.indices(len(self.time_data)))
 
-            trial_subset = zip([self.marker_data[t] for t in trials])
-
-        else:
-            trial_subset = zip(self.marker_data[trials], self.time_data[trials])
-
-        for trial, times in trial_subset:
-            t_indices = (times >= time.start) & (times <= time.stop)
-
-            for trial_marker in trial[markers, :, :]:
-                for trial_marker_dim in trial_marker[dimensions, :]:
-                    plot_list += [times[t_indices], trial_marker_dim[t_indices]]
+        elif not isinstance(trials, (list, tuple, np.ndarray)):
+            raise TypeError("trials must be a  slice, int or iterable of ints")
+        for trial in trials:
+            trial_data = self.marker_data[trial]
+            time_data = self.time_data[trial]
+            t_indices = (time_data >= time.start) & (time_data <= time.stop)
+            for trial_marker_data in trial_data[markers, :, :]:
+                for trial_marker_dim_data in trial_marker_data[dimensions, :]:
+                    plot_list += [time_data[t_indices], trial_marker_dim_data[t_indices]]
 
         return plot_list
 
@@ -60,23 +63,46 @@ class MocapProcessor(object):
         # number of trials will be half of whats left after subtracting start, goal and marker nums
         self.trials = (len(input_data.keys()) - 5) / 2
 
-        self.marker_pos = []
-        self.marker_vel = []
-        self.time_data = []
+        self.marker_pos = OrderedDict()
+        self.marker_vel = OrderedDict()
+        self.time_data = OrderedDict()
 
-        for i in range(self.trials):
-            self.marker_pos.append(input_data['full_sequence_%d' % i])
-            self.time_data.append(input_data['time_%d' % i])
-            self.marker_vel.append(np.diff(self.marker_pos[-1], axis=2) / np.diff(self.time_data[-1]))
+        for trial in range(self.trials):
+            self.marker_pos[trial] = input_data['full_sequence_%d' % trial]
+            self.time_data[trial] = input_data['time_%d' % trial]
+            self.marker_vel[trial] = np.diff(self.marker_pos[trial], axis=2) / np.diff(self.time_data[trial])
 
         self.marker_dict = marker_dict
         self.groups = {}
+        self.plottable_pos = None
+        self.plottable_vel = None
+
+        self._setup_plottables()
+
+    def _setup_plottables(self):
         self.plottable_pos = PlottableMocapData(self.marker_pos, self.time_data, self.start, self.goal)
-        self.plottable_vel = PlottableMocapData(self.marker_vel, [time[1:] for time in self.time_data], self.start,
+        self.plottable_vel = PlottableMocapData(self.marker_vel, [time[1:] for time in self.time_data.values()], self.start,
                                                 self.goal)
+
+    def _recalculate_velocity(self):
+        for trial, pos in self.marker_pos.items():
+            self.marker_vel[trial] = np.diff(pos, axis=2) / np.diff(self.time_data[trial])
 
     def clone(self):
         return deepcopy(self)
+
+    @staticmethod
+    def _parse_component(component):
+
+        if component == '' or component == 'both':
+            pos, vel = True, True
+
+        else:
+            pos = 'pos' in component
+            vel = 'vel' in component
+            assert pos != vel, "Invalid component string! Must be pos(ition), vel(ocity) both (or empty)"
+
+        return pos, vel
 
     def _parse_markers(self, markers):
         marker_list = []
@@ -100,17 +126,60 @@ class MocapProcessor(object):
             try:
                 return self.marker_dict[markers]
 
-            except KeyError:
+            except (KeyError, TypeError):
                 if isinstance(markers, (int, slice)):
                     return markers
 
                 else:
                     raise ValueError("markers must be a slice, int, key or an iterable of keys or ints")
 
-    def __iter__(self):
-        for marker_pos, marker_vel, time_data in zip(self.marker_pos, self.marker_vel, self.time_data):
-            yield marker_pos, marker_vel, time_data
+    def _parse_trials(self, trials):
 
+        if isinstance(trials, int):
+            trials = [trials]
+
+        elif isinstance(trials, slice):
+            trials = range(*trials.indices(self.trials))
+
+        elif not isinstance(trials, (list, tuple, np.ndarray)):
+            raise TypeError("trials must be a  slice, int or iterable of ints")
+
+        return trials
+
+    def _parse_time(self, trial, start_time, stop_time):
+        return (self.time_data[trial] >= start_time) & (self.time_data[trial] <= stop_time)
+
+    def __iter__(self):
+        for trial in self.marker_pos.keys():
+            yield trial, self.marker_pos[trial], self.marker_vel[trial], self.time_data[trial]
+
+    def __getitem__(self, tup):
+        trials, markers, dimensions, time = pad_tuple(tup, *[slice(None)] * 3 + [slice(-np.inf, np.inf)])
+        new_one =  self.clone()
+        if not isinstance(time, slice):
+            raise TypeError("Time indexing must be via a slice!")
+
+        if isinstance(markers, int):
+            markers = slice(markers, markers+1)
+
+        if isinstance(dimensions, int):
+            dimensions = slice(dimensions, dimensions+1)
+
+        trials = self._parse_trials(trials)
+
+        new_one.marker_pos = OrderedDict()
+        new_one.marker_vel = OrderedDict()
+        new_one.time_data = OrderedDict()
+
+        for trial in trials:
+            t_indices = self._parse_time(trial, time.start, time.stop)
+            new_one.marker_pos[trial] = self.marker_pos[trial][markers, dimensions, t_indices]
+            new_one.marker_vel[trial] = self.marker_vel[trial][markers, dimensions, t_indices]
+            new_one.time_data[trial] = self.time_data[trial][t_indices]
+
+        new_one.trials = len(trials)
+        new_one._setup_plottables()
+        return new_one
 
     def assign_group(self, group_name, markers):
         self.groups[group_name] = self.groups.get(group_name, set()) | set(markers)
@@ -118,36 +187,96 @@ class MocapProcessor(object):
     def get_group(self, group_name):
         return self.groups[group_name]
 
-    def plot_pos(self, ax, trials=slice(None), markers=None, start_time=None, stop_time=None):
+    def plot_pos(self, ax, trials=slice(None), markers=slice(None), start_time=None, stop_time=None):
         markers = self._parse_markers(markers)
         time = slice(start_time, stop_time)
         ax.plot(*self.plottable_pos[trials, markers, :, time])
 
-    def plot_vel(self, ax, trials=slice(None), markers=None, start_time=None, stop_time=None):
+    def plot_vel(self, ax, trials=slice(None), markers=slice(None), start_time=None, stop_time=None):
         markers = self._parse_markers(markers)
         time = slice(start_time, stop_time)
-        ax.plot(*self.plottable_pos[trials, markers, :, time])
+        ax.plot(*self.plottable_vel[trials, markers, :, time])
 
     def plot_pos_vel(self, ax_pos, ax_vel, markers):
         self.plot_pos(ax_pos, markers)
         self.plot_vel(ax_vel, markers)
 
-    def velocity_crop(self, max_vel, trials=slice(None), axis=None):
-        cropped = self.clone()
-        for i, (marker_pos, marker_vel, time) in enumerate(cropped):
-            start_index = np.argmax(np.all(marker_vel > max_vel, axis=[0, 1]))
-            stop_index = np.argmax(np.all(marker_vel > max_vel, axis=[0, 1])[::-1])
-            cropped.marker_pos[i] = marker_pos[:,:,start_index:stop_index]
-            cropped.marker_vel[i] = marker_vel[:,:,start_index:stop_index]
-            cropped.time_data[i] = time[start_index:stop_index]
+    def time_slice(self, trial, start_time, stop_time):
+        sliced = self.clone()
+        time = self._parse_time(trial, start_time, stop_time)
+        sliced.marker_pos[trial] = sliced.marker_pos[trial][:, :, time]
+        sliced.marker_vel[trial] = sliced.marker_vel[trial][:, :, time]
+        sliced.time_data[trial] = sliced.time_data[trial][time]
+        return sliced
 
-        cropped.plottable_pos = PlottableMocapData(cropped.marker_pos, cropped.time_data, cropped.start, cropped.goal)
-        cropped.plottable_vel = PlottableMocapData(cropped.marker_vel, [time[1:] for time in cropped.time_data], cropped.start,
-                                                cropped.goal)
+    def velocity_crop(self, max_vel, trials=slice(None), axis=None):
+        trials = self._parse_trials(trials)
+
+        cropped = self.clone()
+        for trial in trials:
+            start_index = np.argmax(np.any(abs(np.nan_to_num(cropped.marker_vel[trial])) > max_vel, axis=(0, 1)))
+            stop_index = np.argmax(np.any(abs(np.nan_to_num(cropped.marker_vel[trial])) > max_vel, axis=(0, 1))[::-1])
+            cropped.marker_pos[trial] = cropped.marker_pos[trial][:, :, start_index:-stop_index]
+            cropped.marker_vel[trial] = cropped.marker_vel[trial][:, :, start_index:-stop_index]
+            cropped.time_data[trial] = cropped.time_data[trial][start_index:-stop_index]
+
+        cropped._setup_plottables()
+        if axis is not None:
+            axis.plot(*cropped.plottable_vel[trials])
 
         return cropped
 
-def unpad(tup, *args):
+    def mutate(self, trials, func, args=None, kwargs=None, indexing=slice(None), component=''):
+        pos, vel = self._parse_component(component)
+
+        if kwargs is None:
+            kwargs = {}
+
+        trials = self._parse_trials(trials)
+
+        for trial in trials:
+            if pos:
+                self.marker_pos[trial][indexing] = func(self.marker_pos[trial][indexing], *args, **kwargs)
+
+            if vel:
+                self.marker_vel[trial][indexing] = func(self.marker_vel[trial][indexing], *args, **kwargs)
+
+    def medfilt(self, trials=slice(None), kernel_size=None, axis=None, component=''):
+        trials = self._parse_trials(trials)
+        pos, vel = self._parse_component(component)
+        filtered = self.clone()
+
+        for trial in trials:
+            markers, dims, _ = filtered.marker_pos[trial].shape
+            for marker in range(markers):
+                for dim in range(dims):
+                    indexing = (marker, dim)
+                    if pos:
+                        filtered.mutate(trial, medfilt, args=(kernel_size,), indexing=indexing, component='pos')
+                        filtered._recalculate_velocity()
+
+                    if vel:
+                        filtered.mutate(trial, medfilt, args=(kernel_size,), indexing=indexing, component='vel')
+
+        if axis is not None:
+            if pos:
+                filtered.plot_pos(axis, trials)
+
+            filtered.plot_vel(axis, trials)
+        return filtered
+
+    def get_npz_data(self):
+        save_data = {}
+        for trial, pos, vel, time in self:
+            save_data['trial_' + str(trial)] = None
+
+
+
+def pad_tuple(tup, *args):
+    try:
+        tup = tuple(tup)
+    except TypeError:
+        tup = (tup, )
     return tup + args[len(tup):]
 
 
@@ -158,9 +287,45 @@ def main():
     args = parser.parse_args()
 
     raw_data = np.load(args.input_data_npz)
+    processor = MocapProcessor(raw_data, {})
+    cropped = processor.medfilt(kernel_size=5)
+    cropped_tmp = cropped.clone()
 
+    for trial, _, _, _ in processor:
+        f, (ax_p, ax_v) = plt.subplots(2, sharex=True)
+        cropped.plot_vel(ax_v, trial)
+        cropped.plot_pos(ax_p, trial)
+        plt.show()
+        max_vel = 0
+        resp = raw_input("Velocity cutoff? [a to accept, m to manually enter times, "
+                         "<ENTER> to use %f again]:\n" % max_vel)
 
+        while resp != 'a':
+            f, (ax_p, ax_v) = plt.subplots(2, sharex=True)
+
+            if resp == 'm':
+                start = float(raw_input("Start time?\n"))
+                stop = float(raw_input("Stop time?\n"))
+                cropped_tmp = cropped.time_slice(trial, start, stop)
+
+            else:
+                max_vel = float(resp)
+                print("velcrop", max_vel)
+                cropped_tmp = cropped.velocity_crop(max_vel, trial, ax_v)
+
+            cropped_tmp.plot_pos(ax_p, trial)
+            plt.show()
+            resp = raw_input("Velocity cutoff? [a to accept, m to manually enter times, "
+                                "<ENTER> to use %f again]:\n" % max_vel)
+
+        cropped = cropped_tmp
 
     with open(args.output_npz, 'w') as output_file:
         np.savez_compressed(output_file)
         print('Task sequences saved to ' + args.output_data_npz)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
